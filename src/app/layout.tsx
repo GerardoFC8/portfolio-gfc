f8cb2a5e75fdce2170808e28d81cbee9ced92a0a
GerardoFC8 <gerardofranco366@gmail.com>
2025-11-06 17:10:48 -0500

    Add SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import ClientLayout from "@/app/client-layout"
 import { createClient } from "@/lib/supabase/server"
 import { cookies } from "next/headers"
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
@@ -50,6 +51,7 @@ export default async function RootLayout({
     <ClientLayout initialLang={langKey} generalText={generalTranslations}>
       {children}
       <Analytics/>
+      <SpeedInsights />
     </ClientLayout>
   )
 }
